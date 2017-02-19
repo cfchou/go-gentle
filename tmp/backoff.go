92c64762eed1a92ee059c86f7eff5642e0444135
cfchou <cfchou@gmail.com>
2017-02-20 00:55:30 +0800

    tmp

---
+++ tmp/backoff.go
@@ -0,0 +1,37 @@
+// vim:fileencoding=utf-8
+package service
+
+import (
+	"time"
+	"gopkg.in/eapache/go-resiliency.v1/retrier"
+)
+
+type DefaultBackOff struct {
+	expBackOff   []time.Duration
+	constBackOff []time.Duration
+}
+
+func NewDefaultBackOff(expInit int, expSteps int, constWindow int) BackOff {
+	return &DefaultBackOff{
+		expBackOff: retrier.ExponentialBackoff(expSteps,
+			IntToMillis(expInit)),
+		constBackOff: retrier.ConstantBackoff(1024,
+			IntToMillis(constWindow)),
+	}
+}
+
+func (b *DefaultBackOff) Run(work func() error) error {
+	retryExp := retrier.New(b.expBackOff, nil)
+	if err := retryExp.Run(work); err == nil {
+		return nil
+	}
+	for {
+		retryConst := retrier.New(b.constBackOff, nil)
+		if err := retryConst.Run(work); err == nil {
+			break
+		}
+	}
+	return nil
+}
+
+
