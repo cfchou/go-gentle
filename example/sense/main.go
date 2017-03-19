12f243aeccff2c3205eaf28845c19e681c3d9226
cfchou <cfchou@gmail.com>
2017-03-19 13:54:57 +0800

    tmp

--- example/sense/main.go
+++ example/sense/main.go
@@ -14,7 +14,11 @@ import (
 	"github.com/rs/xid"
 	"path/filepath"
 	"fmt"
+	"encoding/xml"
+	"bufio"
+	"github.com/pborman/uuid"
 )
+
 
 var (
 	logHandler = log15.MultiHandler(log15.StdoutHandler,
@@ -129,10 +133,96 @@ func (r *RequestProvider) Get() (gentle.Message, error) {
 	}, nil
 }
 
+/*
+<meta-data>
+	<task-id-chain>
+		<task-id>aca4058e-2000-4194-a801-067922ec4968</task-id>
+	</task-id-chain>
+	<user-data>
+		<policy>
+			<policy_domain>default-domain.hes.trendmicro.com</policy_domain>
+			<policy_rules>
+				<policy_rule>d3515bf2-c1ce-4c60-aed8-ff0fe88078fb</policy_rule>
+				<policy_rule>38757cfd-8de7-41b9-a75b-c663e7ca234b</policy_rule>
+				<policy_rule>57f35898-6e42-4a0d-bba3-6180ee960af7</policy_rule>
+				<policy_rule>393c3e59-0e06-4ea4-893e-f91db88c3e2b</policy_rule>
+			</policy_rules>
+		</policy>
+	</user-data>
+</meta-data>
+*/
+
+type TaskIdChain struct {
+	XMLName xml.Name `xml:"task-id-chain"`
+	TaskId []string `xml:"task-id"`
+}
+
+type UserData struct {
+	XMLName xml.Name `xml:"user-data"`
+	Policy *Policy
+}
+
+type Policy struct {
+	XMLName xml.Name `xml:"policy"`
+	PolicyDomain string `xml:"policy_domain"`
+	PolicyRules []string `xml:"policy_rules>policy_rule"`
+}
+
+type MetaData struct {
+	XMLName   xml.Name `xml:"meta-data"`
+	TaskIdChain *TaskIdChain
+	UserData *UserData
+}
 
 func main() {
 	provider := NewRequestProvider("tmp")
 	msg, _ := provider.Get()
 	hmsg := msg.(*hesScanReq)
-	fmt.Println(string(hmsg.content))
+	//fmt.Println(string(hmsg.content))
+	fmt.Println("===============")
+
+
+	var meta_buf bytes.Buffer
+	meta_writer := bufio.NewWriter(&meta_buf)
+
+	//enc := xml.NewEncoder(os.Stdout)
+	//enc := xml.NewEncoder(meta_buf)
+	enc := xml.NewEncoder(meta_writer)
+	userData := &UserData{
+		Policy: &Policy{
+			PolicyDomain:"default-domain.hes.trendmicro.com",
+			PolicyRules: []string{
+				"d3515bf2-c1ce-4c60-aed8-ff0fe88078fb",
+				"38757cfd-8de7-41b9-a75b-c663e7ca234b",
+				"57f35898-6e42-4a0d-bba3-6180ee960af7",
+				"393c3e59-0e06-4ea4-893e-f91db88c3e2b",
+			},
+		},
+	}
+	data := &MetaData{
+		TaskIdChain: &TaskIdChain{
+			TaskId: []string{uuid.New()},
+		},
+		UserData: userData,
+	}
+	meta_writer.WriteString(`<?xml version="1.0"?>`)
+	enc.Encode(data)
+	// Encode calls Flush before returning.
+	// Writer.Flush to calculate current len of meta_buf, which is going to
+	// be embedded in payload.
+	meta_writer.Flush()
+
+	payload := fmt.Sprintf("%08x%s00000002META%08x",
+		len(data.TaskIdChain.TaskId[0]),
+		data.TaskIdChain.TaskId[0], meta_buf.Len())
+
+	meta_writer.WriteString(fmt.Sprintf("TASK%08x", len(hmsg.content)))
+	meta_writer.Write(hmsg.content)
+	meta_writer.WriteString("FFFFFFFF")
+	meta_writer.Flush()
+
+	meta_buf.Bytes()
+	all_bytes := []byte(payload)
+	all_bytes = append(all_bytes, meta_buf.Bytes()[:]...)
+	fmt.Println(string(all_bytes))
 }
