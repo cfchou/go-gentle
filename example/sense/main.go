c5ab1f70481b512872283b2a7bbe3532f6a70f5f
cfchou <cfchou@gmail.com>
2017-03-21 15:51:38 +0800

    ratelimit

--- example/sense/main.go
+++ example/sense/main.go
@@ -21,6 +21,7 @@ import (
 	"sync/atomic"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"strconv"
+	"strings"
 )
 
 const max_int64 = int64(^uint64(0) >> 1)
@@ -30,13 +31,14 @@ var (
 	ErrMessageType = errors.New("Invalid message type")
 
 	// command line options
-	url       = pflag.String("url", "http://127.0.0.1:8080", "HES url")
-	dir       = pflag.String("dir", "mails", "directory contains mails")
-	redis_key = pflag.String("redis-key", "sense", "the key of a set in redis")
-	isDryRun  = pflag.BoolP("dryrun", "d", false, "dry run doesn't send reqests")
+	url             = pflag.String("url", "http://127.0.0.1:8080", "HES url")
+	dir             = pflag.String("dir", "mails", "directory contains mails")
+	db              = pflag.Int("redis-db", 0, "the db used in redis")
+	flush		= pflag.BoolP("redis-flush", "f", false, "clear db in redis")
+	isDryRun        = pflag.BoolP("dryrun", "d", false, "dry run doesn't send requests")
 	max_concurrency = pflag.Int("max-concurrency", 1, "max concurrent requests")
-	max_mails = pflag.Int64("max-mails", max_int64, "max number of emails")
-
+	max_scans = pflag.Int64("max-scans", max_int64, "max scan requests to HES")
+	max_scans_sec	= pflag.Int("max-scans-sec", 1000, "rate limit of max scans per second")
 )
 
 func init() {
@@ -63,24 +65,24 @@ func (m *hesScanResp) Id() string {
 	return m.id
 }
 
-type HesSend struct {
+type HesSendHandler struct {
 	Log    log15.Logger
 	client *http.Client
 	url    string
 	rd	*redis.Client
 }
 
-func NewHesSend(url string, opt *redis.Options) *HesSend {
+func NewHesSendHandler(url string, rd *redis.Client) *HesSendHandler {
 	// https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
-	return &HesSend{
+	return &HesSendHandler{
 		Log:    log.New("mixin", "hes_send"),
 		client: cleanhttp.DefaultPooledClient(),
 		url:    url,
-		rd: redis.NewClient(opt),
+		rd: rd,
 	}
 }
 
-func (s *HesSend) Handle(msg gentle.Message) (gentle.Message, error) {
+func (s *HesSendHandler) Handle(msg gentle.Message) (gentle.Message, error) {
 	s.Log.Debug("Handle() ...", "msg_in", msg.Id())
 	hmsg, ok := msg.(*hesScanReq)
 	if !ok {
@@ -89,49 +91,98 @@ func (s *HesSend) Handle(msg gentle.Message) (gentle.Message, error) {
 			"err", ErrMessageType)
 		return nil, ErrMessageType
 	}
-	//
-	err := s.rd.SAdd(*redis_key, hmsg.Id()).Err()
-	if err != nil {
-		return nil, err
+	/*
+	Create a hash on redis:
+	hmsg.id: {
+		mail_id: "the email's unique file name"
+		scan_req_begin: 1490064094,	// int, redis server's unix timestamp in sec
+
+		// Following are not guaranteed to be available.
+		scan_req_dura: 1.001, 		// float, in sec
+		scan_resp_status: "200"		// string, HTTP response status
+		scan_resp_hes_state: "2"	// string, HES info
+		scan_resp_hes_in: "1"		// string, HES info
+		scan_resp_hes_out: "0"		// string, HES info
+		scan_resp_hes_doing: "0"	// string, HES info
 	}
-	hmsg_data := map[string]string{"mail_id": hmsg.mailId}
-	// use redis' server time as the scan_begin
+	*/
+	scanData := map[string]string{"mail_id": hmsg.mailId}
+	// use redis' server time as scan_req_begin
 	rdtime, err := s.rd.Time().Result()
 	if err != nil {
 		return nil, err
 	}
-	hmsg_data["scan_req_begin"] = strconv.FormatInt(rdtime.Unix(), 10)
+	scanData["scan_mail_sz"] = strconv.Itoa(len(hmsg.content))
+	scanData["scan_req_begin"] = strconv.FormatInt(rdtime.Unix(), 10)
+
+	defer func() {
+		// Send scanData, note some fields might not be available
+		s.Log.Debug("Send to Redis", "msg_in", msg.Id())
+		s.rd.HMSet(hmsg.Id(), scanData).Result()
+	}()
+
 	begin := time.Now()
 	resp, err := s.client.Post(s.url, "application/octet-stream",
 		bytes.NewReader(hmsg.content))
+	timespan := time.Now().Sub(begin)
+	log.Debug("Post() timespan", "msg_in", msg.Id(),
+		"begin", begin.Format(time.StampMilli),
+		"timespan", timespan)
 
-	defer s.rd.HMSet(hmsg.Id(), hmsg_data).Result()
-
-	hmsg_data["scan_req_dura"] = strconv.FormatFloat(
-		time.Now().Sub(begin).Seconds(), 'f',3, 64)
+	scanData["scan_req_dura"] = strconv.FormatFloat(timespan.Seconds(), 'f',
+		3, 64)
 	if err != nil {
 		s.Log.Error("Post() err", "msg_in", msg.Id(), "err", err)
-		hmsg_data["scan_resp_status"] = resp.Status
 		return nil, err
 	}
-	hmsg_data["scan_resp_status"] = resp.Status
-	//fmt.Println(resp.Status)
 
-	body_content, err := ioutil.ReadAll(resp.Body)
-	//fmt.Println(string(body_content))
+	scanData["scan_resp_status"] = resp.Status
+	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		s.Log.Error("ReadAll() err", "msg_in", msg.Id(), "err", err)
 		return nil, err
 	}
-
-	//s.Log.Debug("ReadAll() ok", "msg_in", msg.Id())
+	var hes_state, hes_in, hes_out, hes_doing string
+	body := string(content)
+	if hes_state, ok = parse_hes_info("state", body); ok {
+		scanData["scan_resp_hes_state"]	= hes_state
+	}
+	if hes_in, ok = parse_hes_info("in_queue_size", body); ok {
+		scanData["scan_resp_hes_in"] = hes_in
+	}
+	if hes_out, ok = parse_hes_info("out_queue_size", body); ok {
+		scanData["scan_resp_hes_out"] = hes_out
+	}
+	if hes_doing, ok = parse_hes_info("doing_count", body); ok {
+		scanData["scan_resp_hes_doing"]	= hes_doing
+	}
 	s.Log.Debug("ReadAll() ok", "msg_in", msg.Id(),
-		"resp", string(body_content))
+		"status", resp.Status, "hes_state", hes_state,
+		"hes_in", hes_in, "hes_out", hes_out, "hes_doing", hes_doing)
+
 	return &hesScanResp{
 		id:      msg.Id(),
 		status: resp.Status,
-		content: body_content,
+		content: content,
 	}, nil
+}
+
+func parse_hes_info(key string, body string) (string, bool) {
+	// A legitimate response is like(line-breaks added for readibility):
+	// <p1:common_push_response xmlns:p1="http://www.trendmicro.com/nebula/xml_schema">
+	// <state>2</state>
+	// <in_queue_size>1</in_queue_size>
+	// <out_queue_size>0</out_queue_size><doing_count>0</doing_count>
+	// </p1:common_push_response>
+	key_begin := "<" + key + ">"
+	key_end := "</" + key + ">"
+	i := strings.Index(body, key_begin)
+	si := i + len(key_begin)
+	sj := strings.Index(body, key_end)
+	if i == -1 || si >= sj {
+		return "", false
+	}
+	return body[si:sj], true
 }
 
 type RequestProvider struct {
@@ -142,7 +193,7 @@ type RequestProvider struct {
 	userData *UserData
 }
 
-func NewRequestProvider(dirname string) *RequestProvider {
+func NewRequestStream(dirname string) *RequestProvider {
 	// Reads all files into the memory
 	fs, err := ioutil.ReadDir(dirname)
 	if err != nil {
@@ -262,35 +313,51 @@ func (r *RequestProvider) addMeta(id string, content []byte) []byte {
 func (r *RequestProvider) Get() (gentle.Message, error) {
 	id := uuid.New()
 	mailId := r.names[r.gen.Intn(len(r.names))]
+	content := r.addMeta(id, r.requests[mailId])
+	// correlate msg.Id & mailId in log
+	r.Log.Debug("Get() ok", "msg", id, "mail_id", mailId,
+		"mail_len", len(content))
 	return &hesScanReq{
 		id:      id,
 		mailId:	mailId,
-		content: r.addMeta(id, r.requests[mailId]),
+		content: content,
 	}, nil
 }
 
 func runOne() {
-	provider := NewRequestProvider(*dir)
-	msg, _ := provider.Get()
+	scans := NewRequestStream(*dir)
+	msg, _ := scans.Get()
 	hreq := msg.(*hesScanReq)
 	if *isDryRun {
 		fmt.Println("Is a dry run. Exit")
 		return
 	}
 
-	opt := &redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	}
-
-	hes := NewHesSend(*url + "/scanner/mail", opt)
+	rd := createRedisClient()
+	hes := NewHesSendHandler(*url + "/scanner/mail", rd)
 	hmsg, err := hes.Handle(hreq)
 	if err != nil {
 		log.Error("Handle err", "err", err)
 	}
 	hresp := hmsg.(*hesScanResp)
 	fmt.Println(string(hresp.content))
+}
+
+func runLoop() {
+	scans := NewRequestStream(*dir)
+	rd := createRedisClient()
+	var handler gentle.Handler
+	ratelimit := 1000 / (*max_scans_sec)
+	if ratelimit > 0 {
+		log.Debug("Rate limit enabled, pause in millis between every scan", "pause", ratelimit)
+		handler = gentle.NewRateLimitedHandler("sense",
+			NewHesSendHandler(*url + "/scanner/mail", rd),
+			gentle.NewTokenBucketRateLimit(ratelimit, 1))
+	} else {
+		handler = NewHesSendHandler(*url + "/scanner/mail", rd)
+	}
+	stream := gentle.NewMappedStream("sense", scans, handler)
+	runStream(stream, *max_concurrency, *max_scans)
 }
 
 func runStream(stream gentle.Stream, concurrent_num int, count int64) {
@@ -315,22 +382,27 @@ func runStream(stream gentle.Stream, concurrent_num int, count int64) {
 	}
 }
 
+func createRedisClient() *redis.Client {
+	rd := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       *db,  // use default DB
+	})
+	if (*flush) {
+		rd.FlushDb()
+	}
+	return rd
+}
+
 func main() {
 	h := log15.MultiHandler(log15.StdoutHandler,
 		log15.Must.FileHandler("./test.log", log15.LogfmtFormat()))
 	log.SetHandler(log15.LvlFilterHandler(log15.LvlDebug, h))
-
 	gentle.Log.SetHandler(log15.LvlFilterHandler(log15.LvlDebug, h))
 
-	provider := NewRequestProvider(*dir)
-	opt := &redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	}
-	hes := NewHesSend(*url + "/scanner/mail", opt)
-	stream := gentle.NewMappedStream("sense", provider, hes)
-	go runStream(stream, *max_concurrency, *max_mails)
+	//runOne()
+	go runLoop()
+
 	http.Handle("/metrics", promhttp.Handler())
 	err := http.ListenAndServe(":8080", nil)
 	log.Crit("Promhttp stoped", "err", err)
